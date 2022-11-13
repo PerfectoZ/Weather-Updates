14d0a50fc19518fd6fe63074cf5422ff98746c58
Mandeep Taneja <mandeeptaneja21@outlook.com>
2022-11-13 18:20:58 +0530

    Improve The Format and Content of Email being sent

--- Email.py
+++ Email.py
@@ -1,14 +1,40 @@
-import smtplib
-from datetime import datetime
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+import smtplib
+
+smtp = smtplib.SMTP('smtp.gmail.com', 587)
+smtp.ehlo()
+smtp.starttls()
+
+smtp.login('zalutskiyperfecto@gmail.com','[password]')
 
 recipients = ['mandeeptaneja2000@gmail.com']
 
+def pack(subject="",text="",img=None,attachment=None):
+    msg = MIMEMultipart()
+    msg['Subject']=subject
+    msg.attach(MIMEText(text))
+    if img is not None :
+        if type(img) is not list :
+            img = [img]
+        for i,one_img in enumerate(img) :
+            img_data = open(one_img, 'rb').read()
+            msg.attach(MIMEImage(img_data,name="Img_"+str(i)))
+    if attachment is not None :
+        if type(attachment) is not list :
+            attachment = [attachment]
+        for i,one_attachment in enumerate(attachment) :
+            with open(one_attachment,'rb') as f :
+                file = MIMEApplication(f.read(),name="Attachment_"+str(i))
+            file['Content-Disposition'] = f'attachment'
+            filename="File "+str(i)
+            msg.attach(file)
+    return msg
+
 def SendMail(message=None):
-    server = smtplib.SMTP('smtp.gmail.com',587)
-    server.starttls()
-    message = MIMEText(message).as_string()
-    server.login('zalutskiyperfecto@gmail.com','[password]')
-    for recipient in recipients :
-        server.sendmail('zalutskiyperfecto@gmail.com',recipient,message)
-    server.quit()
+    msg = pack("Weather Updates",message)
+    smtp.sendmail(from_addr="perfectozalutskiy@gmail.com",to_addrs=recipients,msg=msg.as_string())
+    smtp.quit()
+
